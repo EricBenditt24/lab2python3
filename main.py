fc5f9ae9a2085a24c7914c90cf080e78a06735bf
bfd5210 <bfd5210@psu.edu>
2020-09-10 21:28:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Author: Eric Benditt erb5623@psu.edu  
+# Collaborator: Joseph Phillips jmp7146@psu.edu  
+# Collaborator: Kaitlyn Byrnes krb5906@psu.edu
+# Collaborator: Srihulas Nidamanuru sbn5256@psu.edu
+# Section: 4
+# Breakout: 2
+
+
+def grade(): 
+
+  grade = int(input("Enter your CMPSC 131 grade: "))
+if grade >=93 
