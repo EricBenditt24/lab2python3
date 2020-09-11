d2adc48a3bb3ff64c5b3a2a2a90b9ec9a4a5db73
bfd5210 <bfd5210@psu.edu>
2020-09-11 03:16:05 +0000

    I added extra spaces

--- main.py
+++ main.py
@@ -8,45 +8,45 @@
 
 def getLetterGrade(grade): 
   if grade >= 93.0 :
-    return 'A\n'
+    return "A"
   elif grade >= 90.0 :
-    return 'A-\n'
+    return "A-"
   elif grade >= 87.0 :
-    return 'B+\n'
+    return "B+"
   elif grade >= 83.0 :
-    return 'B\n'
+    return "B"
   elif grade >= 80.0 :
-    return 'B-\n'
+    return "B-"
   elif grade >= 77.0 :
-    return 'C+\n'
+    return "C+"
   elif grade >= 70.0 :
-    return 'C\n'
+    return "C"
   elif grade >= 60.0 :
-    return 'D\n'
+    return "D"
   else:
-    return 'F\n'
+    return "F"
 
 def run():
   grade = input("Enter your CMPSC 131 grade: ") 
   numbergrade = float(grade) 
   if numbergrade>=93.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(100.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(100.0)+".")
   elif numbergrade>=90.0 and numbergrade<=93.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(92.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(92.0)+".")
   elif numbergrade>= 87.0 and numbergrade<=90.0: 
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(88.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(88.0)+".")
   elif numbergrade>= 83.0 and numbergrade<=87.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(84.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(84.0)+".")
   elif numbergrade>= 80.0 and numbergrade<=83.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(81.00)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(81.00)+".")
   elif numbergrade>= 77.0 and numbergrade<=80.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(78.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(78.0)+".")
   elif numbergrade>= 70.0 and numbergrade<=77.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(71.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(71.0)+".")
   elif numbergrade>= 60.0 and numbergrade<=70.0:
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(61.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(61.0)+".")
   else: 
-    print("Your letter grade for CMPSC 131 is",getLetterGrade(59.0)+".")
+    print("Your letter grade for CMPSC 131 is", getLetterGrade(59.0)+".")
 
 if __name__ == '__main__':
   run()  
